88b1dfe93a5585b2cf32b659873f70dddea66be4
ChayaSt <chaya.stern@choderalab.org>
2016-10-26 16:03:09 -0400

    started gaussian mixture output model

--- bhmm/output_models/__init__.py
+++ bhmm/output_models/__init__.py
@@ -3,3 +3,4 @@ __author__ = 'noe'
 from bhmm.output_models.outputmodel import OutputModel
 from bhmm.output_models.discrete import DiscreteOutputModel
 from bhmm.output_models.gaussian import GaussianOutputModel
+from bhmm.output_models.gaussian_mix import GaussianMixOutputModel
